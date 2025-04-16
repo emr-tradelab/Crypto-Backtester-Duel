1fcc78005aa3081ecbb727555c556657e35c71a7
Ezequiel Rivero <163421526+ezemriv@users.noreply.github.com>
2025-04-16 13:45:36 +0000

    Temp data setup finished

--- src/config/config.py
+++ src/config/config.py
@@ -1,7 +1,6 @@
 # src/config/config.py
 
 import os
-
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -11,10 +10,16 @@ class CONFIG:
     Configuration class for backtest parameters.
     """
 
-    LOOKBACK_DAYS = 90 #365
+    LOOKBACK_DAYS = 365
     DATA_TIMEFRAME = "1h"
     CCXT_DATA_SYMBOL = "BTC/USDT"
     BINANCE_DATA_SYMBOL = "BTCUSDT"  # Binance format
 
     BINANCE_API_KEY = os.getenv("BINANCE_API_KEY")
     BINANCE_API_SECRET = os.getenv("BINANCE_API_SECRET")
+
+    # Paths
+    CWD = os.path.abspath(os.path.dirname(__file__))
+    ROOT_PATH = os.path.abspath(os.path.join(CWD, "..", ".."))
+    TMP_FILENAME = f"{BINANCE_DATA_SYMBOL}_{DATA_TIMEFRAME}_tmp_data_lookback{LOOKBACK_DAYS}.parquet"
+    DATA_TMP_PATH = os.path.join(ROOT_PATH, "data", TMP_FILENAME)
--- src/main.py
+++ src/main.py
@@ -3,67 +3,27 @@ import time
 from datetime import datetime, timedelta, timezone
 
 from config.config import CONFIG
-from data.binance_api_downloader import BinanceDirectDownloader
-from data.ccxt_data_downloader import ccxtBinanceDataDownloader
-from utils.utils import timeit
+from data.data_pipeline import binance_direct_fetch, save_tmp_data, load_tmp_data
 
+def get_historical_data(download=False):
 
-def calc_dates():
-    """Calculates the start and end timestamps in milliseconds for fetching
-    historical data.
-
-    Returns:
-        tuple[int, int]: A tuple containing the start and end timestamps in
-            milliseconds.
-    """
-    # fetch LOOKBACK_DAYS worth of data
-    end_time = datetime.now(timezone.utc)
-    print(f"End time: {end_time}")
-    start_time = end_time - timedelta(days=CONFIG.LOOKBACK_DAYS)
-    print(f"Start time: {start_time}")
-
-    start_ms = int(time.mktime(start_time.timetuple()) * 1000)
-    end_ms = int(time.mktime(end_time.timetuple()) * 1000)
-
-    return start_ms, end_ms
-
-# @timeit
-# def ccxt_fetch(start_ms, end_ms):
-#     ccxt_client = ccxtBinanceDataDownloader(
-#         api_key=CONFIG.BINANCE_API_KEY, api_secret=CONFIG.BINANCE_API_SECRET
-#     )
-
-#     return ccxt_client.fetch_historical(
-#         symbol=CONFIG.CCXT_DATA_SYMBOL,
-#         timeframe=CONFIG.DATA_TIMEFRAME,
-#         start_ms=start_ms,
-#         end_ms=end_ms,
-#     )
-
-@timeit
-def binance_direct_fetch(start_ms, end_ms):
-    direct_client = BinanceDirectDownloader(
-        api_key=CONFIG.BINANCE_API_KEY, api_secret=CONFIG.BINANCE_API_SECRET
-    )
-    return direct_client.fetch_ohlcv(
-        symbol=CONFIG.BINANCE_DATA_SYMBOL,  # Using "BTCUSDT" vs. "BTC/USDT"
-        interval=CONFIG.DATA_TIMEFRAME,
-        start_ms=start_ms,
-        end_ms=end_ms,
-    )
+    if download:
+        print("\nStarting Direct Binance download ...")
+        df_direct = binance_direct_fetch()
+        print(f"Direct Binance result: {df_direct.head()}")
+        print(f"Direct Binance result shape: {df_direct.shape}")
+        save_tmp_data(df_direct, CONFIG.DATA_TMP_PATH)
+    else:
+        print("\nLoading Direct Binance data from tmp file ...")
+        df_direct = load_tmp_data(CONFIG.DATA_TMP_PATH)
+        print(f"Direct Binance result: {df_direct.head()}")
+        print(f"Direct Binance result shape: {df_direct.shape}")
+    
+    return df_direct
 
 def main():
-    START, END = calc_dates()
 
-    # print("Starting CCXT fetch ...")
-    # df_ccxt = ccxt_fetch(START, END)
-    # print(f"CCXT result: {df_ccxt.head()}")
-    # print(f"CCXT result shape: {df_ccxt.shape}")
-
-    print("\nStarting Direct Binance fetch ...")
-    df_direct = binance_direct_fetch(START, END)
-    print(f"Direct Binance result: {df_direct.head()}")
-    print(f"Direct Binance result shape: {df_direct.shape}")
+    df = get_historical_data(download=False)
 
 if __name__ == "__main__":
     main()
